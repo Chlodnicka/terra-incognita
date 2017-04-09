9730f855305be08119cf361b71bbd4a5377b796d
Chlodnicka <chlodnicka.maja@gmail.com>
2017-04-09 12:46:42 +0200

    Front and Cms controllers and behaviour

---
+++ resources/views/cms/content/media.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.cms-layout')
+
+@section('title', 'Wesprzyj nas')
+
+@section('content')
+
+    <div class="">
+        <div class="col-md-12 col-xs-12">
+            <div class="x_panel">
+                <div class="x_name">
+                    <h2>Wesprzyj nas</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                        <li><a class="close-link" href="/"><i class="fa fa-arrow-left"></i> Wróć</a></li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                    <br/>
+                    <form class="form-horizontal form-label-left" method="post" action="/support"
+                          enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        @foreach ($errors->all() as $error)
+                            <p class="error">{{ $error }}</p>
+                        @endforeach
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Zajawka</label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <input type="text" class="form-control" name="lead" id="lead" value="{{ $content->lead }}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Zdjęcie główne</label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <div class="input-group">
+                           <span class="input-group-btn">
+                             <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                               <i class="fa fa-picture-o"></i> Wybierz
+                             </a>
+                           </span>
+                                    <input id="thumbnail" class="form-control" type="text" name="string_1">
+                                </div>
+                                <img id="holder" style="margin-top:15px;max-height:100px;"
+                                     src="{{ $content->string_1 }}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3 col-sm-3 col-xs-12">Tekst główny</label>
+                            <div class="col-md-9 col-sm-9 col-xs-12">
+                                <textarea cols="50" id="text" name="text" placeholder=""
+                                          rows="4">{!! $content->text !!}</textarea>
+                            </div>
+                        </div>
+
+                        <div class="ln_solid"></div>
+                        <div class="form-group">
+                            <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                                <a href="/" class="btn btn-primary">Anuluj</a>
+                                <input type="submit" class="btn btn-success" value="Wyślij"/>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
---
+++ resources/views/frontend/content/media.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.layout')
+
+@section('title', 'O nas')
+
+@section('content')
+
+    <div class="main-content">
+        <div class="row"><!-- row -->
+
+            <!-- About -->
+            <div class="col-md-12">
+                <h1 class="animbounceInLeft" data-time="600">O nas</h1>
+                @if(!empty($content->lead))<h2 class="animbounceInLeft" data-time="900">{{ $content->lead }}</h2>@endif
+                <div class="devider-page animfadeInLeft" data-time="1100"></div>
+            </div>
+
+            <!-- service -->
+            <div class="col-md-6 spaceup spacedown animfadeInUp" data-time="900">
+                <h4>Grzesiek</h4>
+                <img src="{{ $content->string_2 }}" />
+                <div>{!! $content->long_string_1 !!} </div>
+            </div>
+            <div class="col-md-6 spaceup spacedown animfadeInUp" data-time="1200">
+                <h4>Maciek</h4>
+                <img src="{{ $content->string_1 }}" />
+                <div>{!! $content->text !!}</div>
+            </div>
+
+
+            <!-- About end -->
+        </div><!-- row end -->
+    </div>
+
+@endsection
